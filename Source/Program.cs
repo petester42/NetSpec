736fb3e41e0ab437365996635524508b70309852
Pierre-Marc Airoldi <pierremarcairoldi@gmail.com>
2016-11-24 01:57:46 -0500

    some runner tests

---
+++ Source/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace ConsoleApplication
+{
+    public class Program : IDisposable
+    {
+        public static int Main(string[] args)
+        {
+            using (var program = new Program())
+            {
+                return program.Run(args);
+            }
+        }
+
+        public int Run(string[] args)
+        {
+            try
+            {
+                return 0;
+            }
+            catch
+            {
+                return 1;
+            }
+        }
+
+        private void Tests()
+        {
+            var assembly = Assembly.GetEntryAssembly();
+            var types = assembly.GetTypes().Where(type => type.GetTypeInfo().IsSubclassOf(typeof(NetSpec.NetSpec))).ToList();
+
+            types.ForEach(type =>
+            {
+                var spec = Activator.CreateInstance(type) as NetSpec.NetSpec;
+
+                spec.Run();
+            });
+
+        }
+
+        public void Dispose()
+        {
+
+        }
+    }
+}
